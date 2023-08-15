2df8f4cc703538c0b44d210b19cfc1ee8192aa49
Zeynep Evecen <53823830+reginz@users.noreply.github.com>
2023-08-15 13:56:01 +0300

    carousel functionality

--- script.js
+++ script.js
@@ -41,3 +41,34 @@ function toggleAccordion() {
 }
 
 items.forEach((item) => item.addEventListener("click", toggleAccordion));
+
+const track = document.querySelector(".carousel-track");
+const cards = Array.from(track.children);
+const nextButton = document.getElementById("nextBtn");
+const prevButton = document.getElementById("prevBtn");
+const cardWidth = cards[0].getBoundingClientRect().width;
+
+let currentIndex = 0; // Starting at the 2nd card
+
+updateCarouselPosition();
+
+function updateCarouselPosition() {
+  const moveValue = -(cardWidth + 5) * currentIndex;
+  track.style.transform = `translateX(${moveValue}px)`;
+}
+
+nextButton.addEventListener("click", function () {
+  // Ensure we don't go beyond the 3rd card for the leftmost position
+  if (currentIndex < cards.length - 3) {
+    currentIndex++;
+  }
+  updateCarouselPosition();
+});
+
+prevButton.addEventListener("click", function () {
+  // Ensure we don't go before the 1st card for the leftmost position
+  if (currentIndex > -1) {
+    currentIndex--;
+  }
+  updateCarouselPosition();
+});
